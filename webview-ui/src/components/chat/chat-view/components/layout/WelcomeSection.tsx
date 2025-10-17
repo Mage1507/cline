e66c960b02aef893267404630088358d08643133
Arafatkatze <arafat.da.khan@gmail.com>
2025-10-16 21:44:28 -0700

    Fixing banner to not show CLI release for windows users

--- webview-ui/src/components/chat/chat-view/components/layout/WelcomeSection.tsx
+++ webview-ui/src/components/chat/chat-view/components/layout/WelcomeSection.tsx
@@ -5,6 +5,7 @@ import InfoBanner, { CURRENT_INFO_BANNER_VERSION } from "@/components/common/Inf
 import HistoryPreview from "@/components/history/HistoryPreview"
 import HomeHeader from "@/components/welcome/HomeHeader"
 import { SuggestedTasks } from "@/components/welcome/SuggestedTasks"
+import { PLATFORM_CONFIG, PlatformType } from "@/config/platform.config"
 import { useExtensionState } from "@/context/ExtensionStateContext"
 import { WelcomeSectionProps } from "../../types/chatTypes"
 
@@ -21,13 +22,18 @@ export const WelcomeSection: React.FC<WelcomeSectionProps> = ({
 	taskHistory,
 	shouldShowQuickWins,
 }) => {
-	const { lastDismissedInfoBannerVersion, lastDismissedCliBannerVersion } = useExtensionState()
+	const { lastDismissedInfoBannerVersion, lastDismissedCliBannerVersion, platform } = useExtensionState()
 
 	const shouldShowInfoBanner = lastDismissedInfoBannerVersion < CURRENT_INFO_BANNER_VERSION
 	// const shouldShowNewModelBanner = lastDismissedModelBannerVersion < CURRENT_MODEL_BANNER_VERSION
 
-	// Show CLI banner if not dismissed
-	const shouldShowCliBanner = lastDismissedCliBannerVersion < CURRENT_CLI_BANNER_VERSION
+	const isMacOSOrLinux = platform === "darwin" || platform === "linux"
+
+	// Show CLI banner if not dismissed and platform is VSCode (not JetBrains/standalone)
+	const shouldShowCliBanner =
+		isMacOSOrLinux &&
+		PLATFORM_CONFIG.type === PlatformType.VSCODE &&
+		lastDismissedCliBannerVersion < CURRENT_CLI_BANNER_VERSION
 
 	return (
 		<div className="flex flex-col flex-1 w-full h-full p-0 m-0">
